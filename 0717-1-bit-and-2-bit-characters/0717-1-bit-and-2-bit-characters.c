b58166830a66af1446289c8ace16b053da024b82
Nabhansh <146766007+Nabhansh@users.noreply.github.com>
2026-07-02 10:07:58 +0530

    Time: 0 ms (100%), Space: 9 MB (28.28%) - LeetHub

---
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+
+bool isOneBitCharacter(int* bits, int bitsSize) {
+    int i = 0;
+
+    while (i < bitsSize - 1) {
+        if (bits[i] == 1)
+            i += 2;
+        else
+            i++;
+    }
+
+    return i == bitsSize - 1;
+}
