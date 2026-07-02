c910ce4b8491e380fb1278adcb68d48d47b0c65f
Nabhansh <146766007+Nabhansh@users.noreply.github.com>
2026-07-02 10:09:11 +0530

    Time: 0 ms (100%), Space: 10.3 MB (67.89%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.c
@@ -0,0 +1,17 @@
+int pivotIndex(int* nums, int numsSize) {
+    int total = 0;
+
+    for (int i = 0; i < numsSize; i++)
+        total += nums[i];
+
+    int left = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        if (left == total - left - nums[i])
+            return i;
+
+        left += nums[i];
+    }
+
+    return -1;
+}
