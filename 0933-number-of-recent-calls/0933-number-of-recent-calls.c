a8a342ba05a4db95b056a9e87a9e3cc44d5ea144
Nabhansh <146766007+Nabhansh@users.noreply.github.com>
2026-07-13 20:11:45 +0530

    Time: 16 ms (69.75%), Space: 42.6 MB (24.52%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+typedef struct {
+    int queue[10001];
+    int front;
+    int rear;
+} RecentCounter;
+
+RecentCounter* recentCounterCreate() {
+    RecentCounter* obj = (RecentCounter*)malloc(sizeof(RecentCounter));
+
+    obj->front = 0;
+    obj->rear = 0;
+
+    return obj;
+}
+
+int recentCounterPing(RecentCounter* obj, int t) {
+    obj->queue[obj->rear++] = t;
+
+    while (obj->queue[obj->front] < t - 3000) {
+        obj->front++;
+    }
+
+    return obj->rear - obj->front;
+}
+
+void recentCounterFree(RecentCounter* obj) {
+    free(obj);
+}
