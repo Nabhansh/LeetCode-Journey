a623ad0f8c49d9f5489f8db24d044d69c460f768
Nabhansh <146766007+Nabhansh@users.noreply.github.com>
2026-07-20 21:20:23 +0530

    Time: 0 ms (100%), Space: 20.8 MB (73.53%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.c
+++ 1260-shift-2d-grid/1260-shift-2d-grid.c
@@ -1,43 +1,22 @@
-int** shiftGrid(int** grid, int gridSize, int* gridColSize, int k, int* returnSize, int** returnColumnSizes) {
-    *returnSize = gridSize ;
-    int d = (gridSize)*(*gridColSize) ;
-    k = k%(d) ;
-    int *temp = malloc(k*sizeof(int)) ;
-    *returnColumnSizes = (int*)malloc(gridSize*sizeof(int));
-    int i = 0 ;
-    while(i < gridSize){
-        (*returnColumnSizes)[i] = *gridColSize;
-        i++ ;
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** shiftGrid(
+    int** grid, int gridSize, int* gridColSize, int k,
+    int* returnSize, int** returnColumnSizes
+) {
+    int i, j, m, n=gridSize*gridColSize[0];
+    int w=gridColSize[0], h=gridSize; k%=n;
+
+    *returnColumnSizes=malloc((*returnSize=h)*sizeof(int));
+    int **result=malloc(*returnSize*sizeof(int*));
+
+    for(i=0; i<h; i++) {
+        result[i]=malloc(((*returnColumnSizes)[i]=w)*sizeof(int));
+        for(j=0; j<w; j++){ m=(i*w+j-k+n)%n; result[i][j]=grid[m/w][m%w]; }
     }
-    if(k==0){
-        return grid ;
-    }
-    i = 0 ;
-    while(i < k){
-        temp[i] = grid[i/(*gridColSize)][i%(*gridColSize)] ;
-        i++ ;
-    }
-    i = 0 ;
-    while(i < d){
-        int tem = grid[((i + k)/(*gridColSize))%gridSize][(i + k)%(*gridColSize)] ;
-        grid[((i + k)/(*gridColSize))%gridSize][(i + k)%(*gridColSize)] = *(temp + (i + k)%(k)) ;
-        *(temp + (i + k)%(k)) = tem ; 
-        i++  ;
-    }    
-   int **ans = malloc(gridSize*sizeof(int*)) ;
-   i = 0 ;
-   while(i < gridSize){
-    *(ans + i) = malloc((*gridColSize)*sizeof(int)) ;
-    i++ ;
-   }
-   i = 0 ;
-   while(i < gridSize){
-    int j = 0 ;
-    while(j < *gridColSize){ 
-        *(*(ans + i) + j) = *(*(grid + i)+ j) ;
-        j++ ;
-    }
-    i++ ;
-   }
-   return ans ;
+
+    return result;
 }
