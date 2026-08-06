e528b6c3614d7b025ed68d7284955d54b003bdb6
Nabhansh <146766007+Nabhansh@users.noreply.github.com>
2026-08-06 21:21:42 +0530

    Time: 0 ms (100%), Space: 9 MB (94%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.c
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.c
@@ -1,39 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void write_zero_runtime() {
-    FILE *f = fopen("display_runtime.txt", "w");
-    if (f) {
-        fputs("000", f);
-        fclose(f);
-    }
-}
-
-__attribute__((constructor))
-void init_runtime_trick() {
-    atexit(write_zero_runtime);
-}
-
-/* your actual solution */
-int minCost(int n, int** edges, int edgesSize, int* edgesColSize) {
-    return 0;
-}
-
-int digitProduct(int x) {
+int digitProduct(int n) {
     int product = 1;
-    while (x > 0) {
-        product *= (x % 10);
-        x /= 10;
+    while (n > 0) {
+        product *= (n % 10);
+        n /= 10;
     }
     return product;
 }
 
 int smallestNumber(int n, int t) {
     while (1) {
-        int prod = digitProduct(n);
-        if (prod % t == 0) {
+        if (digitProduct(n) % t == 0)
             return n;
-        }
         n++;
     }
 }
