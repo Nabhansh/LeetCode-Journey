e636359862bbd1bf45179c761503fcac2ee4f5f5
Nabhansh <146766007+Nabhansh@users.noreply.github.com>
2026-08-10 18:48:06 +0530

    Time: 0 ms (100%), Space: 9.1 MB (91.72%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+
+char* restoreString(char* s, int* indices, int indicesSize)
+{
+    char* result = malloc((indicesSize + 1) * sizeof(char));
+
+    for (int i = 0; i < indicesSize; i++)
+    {
+        result[indices[i]] = s[i];
+    }
+
+    result[indicesSize] = '\0';
+
+    return result;
+}
