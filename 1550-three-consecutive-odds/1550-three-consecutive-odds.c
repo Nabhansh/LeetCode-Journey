1d50732eccb317268c1b0c0f0e41a189e275ac85
Nabhansh <146766007+Nabhansh@users.noreply.github.com>
2026-08-11 20:58:20 +0530

    Time: 0 ms (100%), Space: 8.8 MB (42.13%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.c
@@ -0,0 +1,23 @@
+#include <stdbool.h>
+
+bool threeConsecutiveOdds(int* arr, int arrSize)
+{
+    int count = 0;
+
+    for (int i = 0; i < arrSize; i++)
+    {
+        if (arr[i] % 2 != 0)
+        {
+            count++;
+
+            if (count == 3)
+                return true;
+        }
+        else
+        {
+            count = 0;
+        }
+    }
+
+    return false;
+}
