6b92a4458126c7184572a1f5fd526ef821d0a831
Nabhansh <146766007+Nabhansh@users.noreply.github.com>
2026-08-30 10:20:05 +0530

    Time: 0 ms (100%), Space: 8.5 MB (11.71%) - LeetHub

---
+++ 2315-count-asterisks/2315-count-asterisks.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countAsterisks(string s) {
+        int ans = 0;
+        bool inside = false;
+
+        for (char c : s) {
+            if (c == '|')
+                inside = !inside;
+            else if (c == '*' && !inside)
+                ans++;
+        }
+
+        return ans;
+    }
+};
