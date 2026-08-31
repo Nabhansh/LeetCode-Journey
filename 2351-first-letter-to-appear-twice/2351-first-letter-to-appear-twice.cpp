8c7211054b208f5754e025f1638c17f119461e89
Nabhansh <146766007+Nabhansh@users.noreply.github.com>
2026-08-31 21:16:22 +0530

    Time: 0 ms (100%), Space: 8.4 MB (42.44%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        vector<bool> seen(26, false);
+
+        for (char c : s) {
+            if (seen[c - 'a'])
+                return c;
+            seen[c - 'a'] = true;
+        }
+
+        return ' ';
+    }
+};
