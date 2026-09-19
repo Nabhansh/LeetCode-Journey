29d0d729c8a86fe7494c1e801ff751ad722469a1
Nabhansh <146766007+Nabhansh@users.noreply.github.com>
2026-09-19 17:08:58 +0530

    Time: 3 ms (14.81%), Space: 8.5 MB (20.89%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int nextNumber(int n) {
+        int sum = 0;
+        while (n) {
+            int d = n % 10;
+            sum += d * d;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    bool isHappy(int n) {
+        unordered_set<int> seen;
+
+        while (n != 1) {
+            if (seen.count(n))
+                return false;
+
+            seen.insert(n);
+            n = nextNumber(n);
+        }
+
+        return true;
+    }
+};
