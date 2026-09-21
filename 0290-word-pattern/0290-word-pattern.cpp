b7f1f8fe49f9d786df7bb05d79e618d641f91514
Nabhansh <146766007+Nabhansh@users.noreply.github.com>
2026-09-21 18:23:53 +0530

    Time: 0 ms (100%), Space: 8.8 MB (70.61%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        vector<string> words;
+        string word;
+        stringstream ss(s);
+
+        while (ss >> word)
+            words.push_back(word);
+
+        if (pattern.size() != words.size())
+            return false;
+
+        unordered_map<char, string> mp;
+        unordered_map<string, char> rev;
+
+        for (int i = 0; i < pattern.size(); i++) {
+            char c = pattern[i];
+
+            if (mp.count(c) && mp[c] != words[i])
+                return false;
+
+            if (rev.count(words[i]) && rev[words[i]] != c)
+                return false;
+
+            mp[c] = words[i];
+            rev[words[i]] = c;
+        }
+
+        return true;
+    }
+};
